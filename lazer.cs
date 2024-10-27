0b2933e4217ddb2b25b68e4d6448252b197e7f5f
Pglasgow <pglasgow02@qub.ac.uk>
2024-10-27 11:41:25 +0000

    Add files via upload

---
+++ lazer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class lazer : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10.5f; //Lazer speed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Lazer moves in forward direction
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        //Lazer destoyed
+        if(transform.position.z >= 6.5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
